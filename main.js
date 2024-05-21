daf1b67e200e54ba733f1764ea00473004075f72
Mário Fróes (STAINWEGVANHUY) <106205419+froesmario@users.noreply.github.com>
2024-05-21 18:03:18 -0300

    Create main.js
    main teina

---
+++ main.js
@@ -0,0 +1,13 @@
+let nomeUsuário = "" ;
+let elemento = document.querySelector("#nome-usuário");
+
+
+while(nomeUsuário == "") {
+    nomeUsuário = prompt("Qual o seu nome?");
+}
+
+if (nomeUsuário == null) {
+    elemento.textContent = "seja bem-vindo à nossa página";
+} else{
+    elemento.textContent = nomeUsuário;
+}
